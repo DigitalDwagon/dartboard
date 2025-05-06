8c3218e27b20ce2f641671da351d6d1987cdda50
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2025-05-06 10:24:14 -0400

    python is a sane and normal language
    makes UploaderMeta.from_json exist like it was supposed to

--- dartboard/items.py
+++ dartboard/items.py
@@ -4,8 +4,8 @@ import dataclasses_json
 from dataclasses_json import LetterCase, dataclass_json
 
 
-@dataclasses.dataclass
 @dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclasses.dataclass
 class UploaderMeta:
     set_scanner : bool = True
     set_upload_state: bool = False
